3243727477581e7f3afc7485d4ef2940aed48dba
sorenandersen <mail@sorenandersen.com>
2017-03-30 22:16:56 +0200

    npm start script. Port from process.env.PORT.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -20,12 +21,12 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-    // NOTE: no next(); call by intention.
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+//     // NOTE: no next(); call by intention.
+// });
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
@@ -53,4 +54,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
